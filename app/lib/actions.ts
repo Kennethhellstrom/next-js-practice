042cba2af9b2a4bc5e84780ce77845a0e0291920
kenneth hellstrom <kennethhellstrom@hotmail.com>
2024-10-08 00:23:08 -0300

    [Master] - Subindo versão master, projeto finalizado de acordo com curso, corrigindo erros build

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -55,7 +55,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-  } catch (error) {
+  } catch  {
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -94,7 +94,7 @@ export async function updateInvoice(id: string, prevState: State,formData: FormD
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
       `;
-  } catch (error) {
+  } catch  {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
  
@@ -107,7 +107,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice.' };
-  } catch (error) {
+  } catch  {
     return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
