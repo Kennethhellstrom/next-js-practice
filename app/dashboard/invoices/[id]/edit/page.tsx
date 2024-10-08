ea38c36fb70ecbb2ac299ecce5918f90989ddb4d
kenneth hellstrom <kennethhellstrom@hotmail.com>
2024-10-08 01:30:37 -0300

    [Master] - Subindo versão master, projeto finalizado de acordo com curso, corrigindo erros build

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 
 
 
-export default async function Page({ params }: { params: { id: string } }) {
-    const id = await params.id;
+export default async function Page({ params }: { params?: { id?: string } }) {
+    const id = await params?.id || "";
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers(),
